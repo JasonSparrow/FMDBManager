c3849e81a70b0d47114efdf632b66f2a57788f17
小飞hub <2678199009@qq.com>
2017-01-19 09:39:40 +0800

    初始化项目

---
+++ FMDBManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDBManager
+//
+//  Created by 王腾飞 on 2017/1/19.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMDBManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FMDBManager
+//
+//  Created by 王腾飞 on 2017/1/19.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
